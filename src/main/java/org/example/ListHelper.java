d6b675943798012b6a874d2a28a48ac29a38ace7
Niall McGuinness <niall.mcguinness@dkit.ie>
2026-03-18 12:57:08 +0000

    Added ListHelper

---
+++ src/main/java/org/example/ListHelper.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+/*
+ List<String> results = ListHelper.filterBy(names, (String s) -> s.length() > 5);
+ */
+public class ListHelper {
+
+    public static <T> List<T> filterBy(List<T> items, Predicate<T> filter){
+
+        List<T> results = new ArrayList<>();
+
+        //finish this method
+
+        return results;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,33 +2,75 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Predicate;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() throws InterruptedException {
 
-        int intitialCapacity = 20;
-        List<Thread> threads = new ArrayList<Thread>(intitialCapacity);
 
-        //instantiate 3 threads
-        threads.add(new Thread(new NumberThread(1,100, 50)));
-        threads.add(new Thread(new CharThread(65,92, 10)));
-        threads.add(new Thread(new NumberThread(1000,1500, 25)));
 
-        //start 3 threads
-        for(Thread t : threads)
-            t.start();
 
-        //wait for 3 threads to complete
-        for(Thread t : threads)
-            t.join();
 
-        System.out.println("All threads have completed!");
+
+    static void main(/*String[] args*/)  {
+        //how do we make the multi-threaded server respond faster to new clients?
+        //create a pool of threads that we can re-user over time
+        ExecutorService pool = Executors.newCachedThreadPool();
+
+        //notice ppol wraps the runnable in Thread AND it calls Thread.start()
+        pool.submit(new NumberThread(1,100, 50));
+        pool.submit(new Thread(new CharThread(65,92, 10)));
+        pool.submit(new Thread(new NumberThread(1000,1200, 25)));
+
+        //paste in copied code here!
+        pool.shutdown();
+        try {
+            if (!pool.awaitTermination(5, java.util.concurrent.TimeUnit.SECONDS)) {
+                pool.shutdownNow();
+            }
+        }
+        catch (InterruptedException e) {
+            pool.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println("All threads finished!");
     }
 
 
-//    static void main() throws InterruptedException {
+
+    // using lists to wait (i.e. join) for thread completion
+//    static void main(/*String[] args*/) throws InterruptedException {
+//
+//        int intitialCapacity = 20;
+//        List<Thread> threads = new ArrayList<Thread>(intitialCapacity);
+//
+//        //instantiate 3 threads
+//        threads.add(new Thread(new NumberThread(1,100, 50)));
+//        threads.add(new Thread(new CharThread(65,92, 10)));
+//        threads.add(new Thread(new NumberThread(1000,1200, 25)));
+//
+//        //check if threads are daemon (lower priority and terminated on Main.exe termination) or user
+//        for(Thread t : threads)
+//            System.out.println(t.isDaemon());
+//
+//        //start 3 threads
+//        for(Thread t : threads)
+//            t.start();
+//
+//        //wait for 3 threads to complete
+//        for(Thread t : threads)
+//            t.join();
+//
+//        System.out.println("All threads have completed!");
+//    }
+
+
+    //creating three threads and calling join()
+//    static void main(/*String[] args*/) throws InterruptedException {
 //
 //        //create and start a thread
 //        NumberThread r1 = new NumberThread(25, 50, 1);
