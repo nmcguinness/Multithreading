c1d9e19c066ab768177577cfbd78e0757f8ee899
Niall McGuinness <niall.mcguinness@dkit.ie>
2026-03-18 12:10:28 +0000

    Added join

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,17 +6,26 @@ public class Main {
     static void main() throws InterruptedException {
 
         //create and start a thread
-        NumberThread r1 = new NumberThread(25, 50, 100);
+        NumberThread r1 = new NumberThread(25, 50, 1);
         Thread t1 = new Thread(r1);
-        t1.start();
+       // t1.start();
 
-        //create and start another!
-        new Thread(new NumberThread(500, 500, 100)).start();
+        //create and start another BUT we have no thread reference :(
+        new Thread(new NumberThread(500, 500, 124)).start();
 
-        Thread t2 = new Thread(new CharThread(65, 92, 250));
-        t2.start();
+        Thread t2 = new Thread(new CharThread(65, 92, 248));
+        //t2.start();
 
-        System.out.println("done");
+        // put in the start list (i.e. doesnt guarantee that we run!)
+        t1.start();  t2.start();
+
+        //wait for threads to complete!
+        t1.join();  t2.join();
+
+        System.out.println("Finished!");
+
+
+
 
     }
 }
